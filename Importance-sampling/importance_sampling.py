904a492f28cedd2394f96f15f63f574e74187c1c
Sunny Vagnozzi <sunny.vagnozzi@fysik.su.se>
2016-12-06 15:07:58 +0100

    Update importance_sampling.py

--- Importance-sampling/importance_sampling.py
+++ Importance-sampling/importance_sampling.py
@@ -1,1 +1,16 @@
+import numpy
+from numpy import *
+tauold = 0.058
+sigmaold = 0.012
+taunew = 0.055
+sigmanew = 0.009
 
+print "Enter the chain root"
+root = raw_input()
+
+for i in range (1,7):
+    numchain = str(i)
+    mychain = loadtxt(root+'_'+numchain+'.txt')
+    mychain[:,0] = mychain[:,0]*(numpy.exp(-((mychain[:,5]-taunew)/sigmanew)**(2.0))/numpy.exp(-((mychain[:,5]-tauold)/sigmaold)**(2.0)))
+    numpy.savetxt(root+'_'+numchain+'.txt',mychain,fmt='%15.8e')
+    del mychain
